bd64fa4a622eabc946a9fd18d7410a5c72258bfc
IyXerXez72 <51115343+IyXerXez72@users.noreply.github.com>
2019-05-28 17:31:32 +0700

    Add files via upload

---
+++ h.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python2
+
+
+#Compiled InYoyurXerXez7
+#2e4hTeam
+#Kiya
+import os
+import sys
+import time
+from time import sleep
+
+g = "\033[32;1m"
+gt = "\033[0;32m"
+bt = "\033[34;1m"
+b = "\033[36;1m"
+m = "\033[31;1m"
+c = "\033[0m"
+p = "\033[37;1m"
+u = "\033[35;1m"
+M = "\033[3;1m"
+k = "\033[33;1m"
+kt = "\033[0;33m"
+a = "\033[30;1m"
+
+W = "\x1b[0m"
+R = "\x1b[31m"
+G = "\x1b[1;32m"
+O = "\x1b[33m"
+B = "\x1b[34m"
+P = "\x1b[35m"
+C = "\x1b[36m"
+GR = "\x1b[37m"
+def slowprints(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(2.0/90)
+
+
+gilang=(g+"""
+|SCRIPT UNTUK 》CTRL TERMUX
+|SINCE        》2k19 - 2k20
+|EXTENSI      》Python - Bash
+|CODING BY    》IyXerXez7
+              》JANGAN ANGGAP ORANG LAIN SAMPAH
+              》JIKA DIRIMU SAJA SEPERTI SAMPAH
+              》
+              》MERENDAH LAH AN TERUS MERENDAH
+              》SAMPAAI KAMU TIDA BISA LAGI DIRENDAHKAN
+______________________
+|{ I }   { X }  { L }|
+----------------------
+(I) = INFO
+(X) = EXIT
+(L) = LANJUT
+     😂😂😂MASUKAN YANG KAMU INGINKAN X/I/L😂😂😂
+CONTACK : 085886686741
+FB      : maklli maenFf
+""")
+slowprints(gilang)
